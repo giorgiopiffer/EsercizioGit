57a8a30ad720280d2744125bb2c7c9c9aa232c38
piffer <pippo@pippo.it>
2018-12-13 20:09:37 +0100

    Inserito somma.c e modificato main.c aggiungendo una nuova printf

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main ()
 {
 
 printf("CIOA A TUTTI-modificato");
+printf("Aggiunta una nuova riga di saluto; ma che fatica !!!);
 
 return 0;
 }
---
+++ somma.c
@@ -0,0 +1,19 @@
+// Nome programma = somma.c
+// Programma che somma 2 numeri
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+
+int a=0;
+int b=3;
+int c=5;	
+
+a = b + c;
+
+printf("Risultato =%d\n", a);
+
+return 0;
+}
+
